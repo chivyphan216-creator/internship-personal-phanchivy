3c495db929708a3633e8ba45808974ba99f44832
unknown <chivyphan216@gmail.com>
2026-08-29 13:56:01 +0700

    fix: cast libsql and adapter types

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -7,12 +7,12 @@ const libsql = createClient({
   authToken: process.env.TURSO_AUTH_TOKEN,
 })
 
-const adapter = new PrismaLibSql(libsql)
+const adapter = new PrismaLibSql(libsql as any)
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({ adapter })
+  new PrismaClient({ adapter: adapter as any })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
