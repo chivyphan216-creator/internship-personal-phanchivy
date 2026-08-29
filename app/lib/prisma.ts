5928263cdef0d3d7d824064e243c421b1a9a591a
unknown <chivyphan216@gmail.com>
2026-08-29 13:54:09 +0700

    fix: update schema to use env DATABASE_URL and driverAdapters

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,15 +1,18 @@
 import { PrismaClient } from '@prisma/client'
 import { PrismaLibSql } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
 
-const adapter = new PrismaLibSql({
-  url: process.env.DATABASE_URL ?? '',
+const libsql = createClient({
+  url: process.env.DATABASE_URL || '',
   authToken: process.env.TURSO_AUTH_TOKEN,
 })
+
+const adapter = new PrismaLibSql(libsql)
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({ adapter: adapter as any })
+  new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
