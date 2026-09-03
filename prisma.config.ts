9310eba98e687025e888ce99ea7b9eaa24c4bead
unknown <chivyphan216@gmail.com>
2026-09-03 21:21:32 +0700

    Fix Prisma v7 datasource config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from '@prisma/config';
-import 'dotenv/config';
+import "dotenv/config";
+import { defineConfig } from "@prisma/config";
 
 export default defineConfig({
-  schema: './prisma/schema.prisma',
+  earlyAccess: true,
   datasource: {
     url: process.env.DATABASE_URL,
   },
