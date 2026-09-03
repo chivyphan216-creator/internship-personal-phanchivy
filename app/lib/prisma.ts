a218372c66c86d683f090ade66e908de5d38f235
unknown <chivyphan216@gmail.com>
2026-09-03 20:46:58 +0700

    Fix typescript pool config error for neon adapter

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -2,12 +2,12 @@ import { PrismaClient } from '@prisma/client'
 import { PrismaNeon } from '@prisma/adapter-neon'
 import { Pool } from '@neondatabase/serverless'
 
-const connectionString = process.env.DATABASE_URL
+const connectionString = process.env.DATABASE_URL!
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 const pool = new Pool({ connectionString })
-const adapter = new PrismaNeon(pool)
+const adapter = new PrismaNeon(pool as any)
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
 
