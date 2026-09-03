9db1b1e8ffbbd1e627da47cab2f6c51bd83c5e59
unknown <chivyphan216@gmail.com>
2026-09-03 20:14:48 +0700

    Fix prisma client initialization with Neon driver adapter

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,9 +1,14 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import { Pool } from '@neondatabase/serverless'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const connectionString = process.env.DATABASE_URL
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient();
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+const pool = new Pool({ connectionString })
+const adapter = new PrismaNeon(pool)
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
